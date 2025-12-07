d9d858e32c327cc6f8c906bd25af7cbacfcb698b
osenpb <oeperezbellido@gmail.com>
2025-12-07 11:56:10 -0500

    first commit

---
+++ favorites_spotify_backend/src/main/java/com/osen/favorites_spotify_backend/FavoritesSpotifyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.osen.favorites_spotify_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FavoritesSpotifyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FavoritesSpotifyBackendApplication.class, args);
+	}
+
+}
